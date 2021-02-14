7892bdfcd585e264cfa5735e063046a16125bede
SashaZ <sasha.zahnitko@gmail.com>
2021-02-14 14:07:56 +1000

    Initial commit for the start-your-ngines branch.
    Added the basic routing for the client and the server.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res, next) => res.status(202));
+app.get("/", (req, res, next) => res.status(202).json({ message: "Hello" }));
 
 app.listen(5000, () => console.log("Listening"));
